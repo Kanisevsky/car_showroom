6c79f5f5d87fb7e4a76230eee4f1ad9fbd960cf5
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-03 19:03:14 +0000

    Finishing Hero Section and adding Navbar and Footer to the layout by that also finishing the layout of the page

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,10 +1,9 @@
 "use client"
-import React from 'react'
-import Image from 'next/image'
+
 import { CustomButtonProps } from '@/app/types'
-function CustomButton({title, containerStyles, handleClick}: CustomButtonProps) {
+function CustomButton({title, btnType, containerStyles, handleClick}: CustomButtonProps) {
   return (
-    <button disabled={false} type={"button"} className={`custom-btn ${containerStyles}`} onClick={handleClick} >
+    <button disabled={false} type={btnType || "button"} className={`custom-btn ${containerStyles}`} onClick={handleClick} >
       <span className={`flex-1`}>{title}</span>
     </button>
   )
