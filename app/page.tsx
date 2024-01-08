f8132a4bae3f8fa3610d0af58ad2100f0b1f38ce
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-08 19:02:04 +0000

    adding showmore component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-import {Hero,SearchBar, CustomFilter, CarCard} from '@/components'
+import {Hero,SearchBar, CustomFilter, CarCard, ShowMore} from '@/components'
 import { fetchCars } from '@/utils'
 import { fuels, yearsOfProduction } from '@/constants';
 
@@ -35,6 +34,11 @@ export default async function Home({searchParams}) {
                 <CarCard  car={car}/>
               ))}
             </div>
+            <ShowMore 
+            pageNumber={(searchParams.pageNumber || 10) / 10}
+            isNext={searchParams.limit || 10  > allCars.length}
+            />
+
           </section>
         ) : (
           <div className='home__error-container'>
--- components/index.ts
+++ components/index.ts
@@ -7,6 +7,7 @@ import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard";
 import CardDetails from "./CardDetails";
+import ShowMore from "./ShowMore";
 export {
   Hero,
   CustomButton,
@@ -17,4 +18,5 @@ export {
   SearchManufacturer,
   CarCard,
   CardDetails,
+  ShowMore,
 };
