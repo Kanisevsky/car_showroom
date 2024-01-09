0e44867f69a6912b812e017e8838b789f6278dc5
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-09 19:09:11 +0000

    showMore component

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,8 +1,24 @@
+"use client"
+import { ShowMoreProps } from "@/app/types"
+import { useRouter } from "next/navigation"
+import { CustomButton } from "."
+import { updateSearchParams } from "@/utils"
 
-function ShowMore() {
+function ShowMore({pageNumber, isNext} : ShowMoreProps) {
+
+  const router = useRouter()
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10
+    const newPathName = updateSearchParams("limit", `${newLimit}` )
+    router.push(newPathName)
+  }
   return (
-    <div>
-      
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton title="Show More" btnType="button" 
+        containerStyles="bg-primary-blue rounded-full text-white" 
+        handleClick={handleNavigation}/>
+      )}
     </div>
   )
 }
